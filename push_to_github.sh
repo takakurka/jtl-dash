ee6b23863c688a25dd4b2a08bebe05edc27a3888
takakurka <sylwia.graphics@gmail.com>
2025-06-25 16:35:14 +0200

    Add files via upload

---
+++ push_to_github.sh
@@ -0,0 +1,7 @@
+cd /mnt/data/jtl_dashboard
+git init
+git remote add origin https://github.com/takakurka/jtl-dashboard.git
+git add .
+git commit -m "Initial dashboard commit"
+git branch -M main
+git push -u origin main
